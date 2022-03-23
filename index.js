81d8dc12e9202a8c5cbc229e34165d3151ea9fd5
antran1245 <antran1245@gmail.com>
2022-03-23 16:57:37 -0700

    completed

---
+++ index.js
@@ -0,0 +1,36 @@
+// Part 1
+for(let i = 1; i < 20; i++) {
+    if(i%2 != 0) { //check if odd
+        console.log(i)
+    }
+}
+
+//Part 2
+let num = 100;
+while (num != 0) {
+    if(num%3 == 0) {
+        console.log(num);
+    }
+    num--;
+}
+
+//Part 3
+let num = 4;
+while(num > -4) {
+    console.log(num);
+    num-=1.5;
+}
+
+//Part 4
+let sum = 0;
+for(let i = 1; i < 101; i++) {
+    sum += i;
+}
+console.log(sum);
+
+//Part 5
+let product = 1;
+for(let i = 1; i < 13; i++) {
+    product *= i;
+}
+console.log(product);
